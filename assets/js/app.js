9c610b08db07b5c6c1c63fa128b6ea8e1b2f848e
Teodor <preprint@pensoft.net>
2024-01-30 14:46:25 +0200

    latest

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,52 +39,29 @@ $(document).ready(function() {
 
     function updateLanguageSwitcherLinks(currentLang) {
         var regex = new RegExp('^(\/' + currentLang + ')(\/|$)');
-        var hash = window.location.hash; // Get the current hash
-
-
+        var hash = window.location.hash; 
+    
+        $('.language-switcher .language').removeClass('active');
+    
         $('.language-switcher .language').each(function() {
             var lang = $(this).data('lang');
-
-            var newPath = window.location.pathname.replace(regex, '\/' + lang + '\/');
+            var newPath = window.location.pathname.replace(regex, '/' + lang + '/');
+    
             if (!window.location.pathname.match(regex)) {
                 newPath = '/' + lang + window.location.pathname;
             }
-            $(this).attr('href', newPath);
-
-            // Highlight the active language
+    
+            newPath = newPath.replace(/\/\/+/g, '/');
+    
+            $(this).attr('href', newPath + hash);
+    
             if (lang === currentLang) {
                 $(this).addClass('active');
-            } else {
-                $(this).removeClass('active');
             }
         });
-
-        // $('.language-switcher .language').each(function() {
-        //     var lang = $(this).data('lang');
-
-        //     if (lang === currentLang) {
-        //         $(this).addClass('active');
-        //     } else {
-        //         $(this).removeClass('active');
-        //     }
-
-        //     var urlSegments = window.location.pathname.split('/');
-        //     var hasMembers = (urlSegments[1] === 'members' || urlSegments[2] === 'members');
-
-        //     var newPath = window.location.pathname.replace(regex, '/' + lang + '/');
-        //     if (!window.location.pathname.match(regex)) {
-        //         newPath = '/' + lang + window.location.pathname;
-        //     }
-
-        //     if (hasMembers) {
-        //         newPath = newPath.replace(/\/\/+/g, '/');
-        //         $(this).attr('href', newPath + (hasMembers ? window.location.hash : ''));
-        //     }
-        // });
     }
-    // update switcher
-    updateLanguageSwitcherLinks(currentLang);
 
+    updateLanguageSwitcherLinks(currentLang);
 
 
     /*
@@ -111,8 +88,10 @@ $(document).ready(function() {
     function showTab(target) {
         $('.partner-institutions, .governing-board, .executive-board').hide();
         $(target).show();
+        // Remove active class from all tab links
         $('.tab-link').removeClass('active');
-        $('a[href$="' + target + '"]').addClass('active');
+        // Add active class only to tab links that have href ending with the target and are not part of the language switcher
+        $('.tab-link:not(.language-switcher .language)').filter('[href$="' + target + '"]').addClass('active');
     }
 
     showTabFromHash();
@@ -122,13 +101,18 @@ $(document).ready(function() {
 
     updateTabLinks();
 
+    // Bind the click event to tab links and update the language switcher accordingly
     $('.tab-link').on('click', function(e) {
         e.preventDefault();
         var hash = $(this).attr('href').split('#')[1];
         window.location.hash = '#' + hash;
         showTab('#' + hash);
         window.scrollTo(0, 0);
+
+        // Update the language switcher links with the new hash
+        updateLanguageSwitcherLinks(currentLang);
     });
+
 
 
     /*
@@ -136,24 +120,27 @@ $(document).ready(function() {
     **/
     $('.full-description-toggle').click(function(e) {
         e.preventDefault();
-
+    
         var $this = $(this);
         var $parentDiv = $this.closest('.member-card, .institutions');
         var $shortDescription = $parentDiv.find('.member-description, .institution-description');
         var $fullDescription = $parentDiv.find('.institution-full');
         var $toggleText = $this.find('.toggle-text');
-
+    
         var readMoreText = $this.data('read-more');
         var hideText = $this.data('hide');
-
-        $shortDescription.toggle('slow');
-        $fullDescription.toggle('slow');
-
-        if ($fullDescription.is(':visible')) {
-            $toggleText.text(hideText);
-        } else {
-            $toggleText.text(readMoreText);
-        }
+    
+        $shortDescription.toggle('slow', function() {
+            //animation complete
+        });
+        $fullDescription.toggle('slow', function() {
+            if ($fullDescription.is(':visible')) {
+                $toggleText.text(hideText);
+            } else {
+                $toggleText.text(readMoreText);
+            }
+        });
+    
         $this.toggleClass('toggled');
     });
 
